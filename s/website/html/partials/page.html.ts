99b0affad2db4651004c01a3e1a2d78f2961783e
Chase Moskal <chasemoskal@gmail.com>
2022-06-12 00:49:02 -0700

    hotfix: upgrade to fail-tolerant xiome

--- s/website/html/partials/page.html.ts
+++ s/website/html/partials/page.html.ts
@@ -34,10 +34,12 @@ export default ({v, mode, pageName, pageSubtitle, headHtml, mainHtml}: NceWebsit
 
 	${mode === "debug"
 		? html`
-			<xiome-mock></xiome-mock>
+			<xiome-config
+				app=ea296d14b6d75503a7d419da911c64c1a8d685c3cc6739d2483bccb3a6af973c
+			></xiome-config>
 			<script type=importmap-shim src="/importmap.json"></script>
 			<script defer type=module-shim src="/main.js"></script>
-			<script defer type=module-shim src="/node_modules/xiome/x/xiome-mock.js"></script>
+			<script defer type=module-shim src="/node_modules/xiome/x/xiome.js"></script>
 			<script defer src="/node_modules/es-module-shims/dist/es-module-shims.js"></script>
 		`
 		: html`
