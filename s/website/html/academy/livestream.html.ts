e7c2ce70086fbf3149ffaa3641da47d6a57df4d3
Chase Moskal <chasemoskal@gmail.com>
2023-03-15 19:42:08 -0700

    tweak: livestream message

--- s/website/html/academy/livestream.html.ts
+++ s/website/html/academy/livestream.html.ts
@@ -21,8 +21,8 @@ mainHtml: html`
 				<div class="boxes">
 					<div class="videobox">
 						<xiome-video-display initially-hidden>
-							<p slot="unavailable">livestream is coming soon</p>
-							<p slot="unprivileged">your account does not have permission to view the livestream</p>
+							<p slot="unavailable">livestream is currently unavailable.</p>
+							<p slot="unprivileged">the livestream is either off, or your account doesn't have permission to view it.</p>
 						</xiome-video-display>
 					</div>
 					<div class="chatbox">
