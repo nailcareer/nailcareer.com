a6712330d884dc0973580a43333f7536fea6ead4
Chase Moskal <chasemoskal@gmail.com>
2023-03-22 23:19:36 -0700

    add: note_coming_soon on membership page

--- s/website/html/academy/index.html.ts
+++ s/website/html/academy/index.html.ts
@@ -26,8 +26,8 @@ mainHtml: html`
 				</div>
 				<div class=cell>
 					<ul>
-						<li>Gain access to Suzie's monthly <a href="./livestream">Livestream</a></li>
 						<li>Post questions for Suzie to answer on the <a href="./qa">Q&amp;A board</a></li>
+						<li><span class=note_coming_soon>Coming Soon</span> Gain access to Suzie's monthly <a href="./livestream">Livestream</a></li>
 					</ul>
 				</div>
 			</div>
