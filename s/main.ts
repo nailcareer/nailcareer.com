4d4c61d5e67fb5c3088bf3d4b309a99d8d120a4b
Chase Moskal <chasemoskal@gmail.com>
2023-08-07 14:11:53 -0700

    create: shell of store2

--- s/main.ts
+++ s/main.ts
@@ -18,4 +18,16 @@ registerComponents(
 import "shopper/x/install-shopper.js"
 import "carouse"
 
+import {install_sheep_cart, register_to_dom} from "sheep-cart"
+
+const {elements, load} = install_sheep_cart({
+	domain: "dev-bakery.myshopify.com",
+	storefront_access_token: "5f636be6b04aeb2a7b96fe9306386f25",
+})
+
+register_to_dom(elements)
+
+await load()
+
 console.log("💅")
+
---
+++ s/website/html/store2/index.html.ts
@@ -0,0 +1,41 @@
+
+
+import pageHtml from "../partials/page.html.js"
+import {html} from "xiome/x/toolbox/hamster-html/html.js"
+import {NceWebsiteContext} from "../../types.js"
+
+export default ({base, mode, ...context}: NceWebsiteContext) => pageHtml({
+...context,
+base,
+mode,
+pageName: "store",
+pageSubtitle: "Store",
+mainHtml: html`
+
+	<sheep-modal></sheep-modal>
+
+	<sheep-cart-modal>
+		<sheep-cart></sheep-cart>
+	</sheep-cart-modal>
+
+	<div class="follower slice">
+		<sheep-cart-button></sheep-cart-button>
+	</div>
+
+	<header class="header slice">
+		<div class=bigbar>
+			<h1>
+				<a href="https://github.com/chase-moskal/sheep-cart">
+					<span>sheep-cart</span>
+					<small>(on github)</small>
+				</a>
+			</h1>
+			<sheep-search></sheep-search>
+			<sheep-nav></sheep-nav>
+		</div>
+	</header>
+
+	<sheep-catalog class=slice></sheep-catalog>
+
+`})
+
